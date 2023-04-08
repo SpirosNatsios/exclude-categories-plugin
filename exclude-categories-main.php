1bf412bc9bbc6a0781dcce462a84612650998b10
Spiros Natsios <spi.natsios@gmail.com>
2023-04-08 15:11:02 +0300

    Include necessary files

---
+++ exclude-categories-main.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Exclude Categories
+ * Description: 
+ * Version: 1.0
+ * Author: Spiros Natsios
+ * Author URI: https://theblink.gr/
+ **/
+
+
+include_once('exclude-categories-functions.php');
+include_once('includes/menu-functions.php');
+include_once('includes/options-pages.php');
+include_once('includes/shop-widget-options-page.php');
+ 
+ ?>
