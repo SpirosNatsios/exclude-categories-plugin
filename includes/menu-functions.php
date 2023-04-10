353f93ae6d0ad0cf0a21479862b29c9e5db0dcff
Spiros Natsios <spi.natsios@gmail.com>
2023-04-10 14:15:54 +0300

    Add shop page sub-menu and modify top-level menu

--- includes/menu-functions.php
+++ includes/menu-functions.php
@@ -6,11 +6,12 @@ add_action('admin_init', 'exclude_categories_register_settings');
 function exclude_categories_add_menu()
 {
     add_menu_page(
-        'Excluded Categories',
-        'Excluded Categories',
+        'Exclude Categories',
+        'Exclude',
         'manage_options',
         'exclude-categories',
-        'exclude_categories_options_page'
+        'exclude_categories_options_page',
+        'dashicons-superhero-alt'
     );
 
     add_submenu_page(
@@ -20,6 +21,15 @@ function exclude_categories_add_menu()
         'manage_options',
         'shop-page-widget',
         'shop_page_widget_options_page'
+    );
+
+    add_submenu_page(
+        'exclude-categories',
+        'Shop Page',
+        'Shop Page',
+        'manage_options',
+        'shop-page',
+        'shop-page-options-page'
     );
 }
 
