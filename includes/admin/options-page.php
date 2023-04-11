ebc41f1e8ce04c1182717b9ccb666cef6fef74e0
Spiros Natsios <spi.natsios@gmail.com>
2023-04-11 16:45:10 +0300

    Add correct urls and button names

--- includes/admin/options-page.php
+++ includes/admin/options-page.php
@@ -11,9 +11,9 @@ function exclude_categories_options_page()
         <p>Please select the option from below where you want to exclude the categories:</p>
         <br>
         <div class="options-buttons">
-            <a class="button button-primary" href="<?php echo admin_url('edit-tags.php?taxonomy=product_cat&post_type=product'); ?>">Exclude categories from specific products</a>
             <a class="button button-primary" href="<?php echo admin_url('admin.php?page=shop-page'); ?>">Exclude categories from Shop page</a>
             <a class="button button-primary" href="<?php echo admin_url('admin.php?page=shop-page-widget'); ?>">Exclude categories from Shop page widget</a>
+            <a class="button button-primary" href="<?php echo admin_url('admin.php?page=related-products'); ?>">Exclude categories from Related Products</a>
         </div>
     </div>
     <?php
