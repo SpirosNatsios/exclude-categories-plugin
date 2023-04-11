2dd55fc10919e60d97dafa8e1ff5a3e327e52c38
Spiros Natsios <spi.natsios@gmail.com>
2023-04-11 16:43:21 +0300

    Add function to exclude from related

--- includes/exclude-categories-functions.php
+++ includes/exclude-categories-functions.php
@@ -7,6 +7,7 @@ function exclude_categories_from_product_categories_widget( $args ) {
                 return $args;
 }
 
+
 function exclude_product_category_from_shop_page( $q ) {
          $tax_query = (array) $q->get( 'tax_query' );
         $excluded_categories = get_option( 'shop_page_excluded_categories', array() );
@@ -17,10 +18,42 @@ function exclude_product_category_from_shop_page( $q ) {
             'terms' => $excluded_categories,
             'operator' => 'NOT IN'
         );
+        
     
         $q->set( 'tax_query', $tax_query );
     }
 
+
+    function exclude_product_category_from_related_products($related_posts, $product_id, $args){
+
+        $term_ids = get_option('related_products_excluded_categories', array());
+
+        if (!empty($term_ids)){
+            $excluded_categories = wc_get_products( array(
+                'status' => 'publish',
+                'limit' => -1,
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'product_cat',
+                        'field' => 'term_id',
+                        'terms' => $term_ids,
+                        'operator' => 'IN'
+                    ),
+                ),
+                'return' => 'ids',
+            ));
+        }
+        else {
+            $excluded_categories = array();
+        }
+
+
+        return array_diff($related_posts,$excluded_categories);
+    }
+    
+
     add_filter( 'woocommerce_product_categories_widget_args', 'exclude_categories_from_product_categories_widget' );
     add_action( 'woocommerce_product_query', 'exclude_product_category_from_shop_page',999 );  
+    add_filter( 'woocommerce_related_products', 'exclude_product_category_from_related_products', 10, 3 );
+
 ?>
